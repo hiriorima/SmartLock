63280f2d2c684573df2ae6cb81c1878c3eb548f3
hiriorima <g2117001@fun.ac.jp>
2017-11-09 12:35:33 +0900

    Add status validation

--- servo/serializer.py
+++ servo/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Servo
 
-
 class ServoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servo
         fields = ('status', 'created_at',)
+        extra_kwargs = {'status': {'required': 'True'}}
+        validators = []
+
+    def validate_status(self, value):
+        data = self.get_initial()
+        status = data.get('status')
+
+        if status != 'open' and status != 'close':
+           raise serializers.ValidationError("incorrect status parameters")
+        return value
