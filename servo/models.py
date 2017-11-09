5939ac48f69a7729376b4bd04cbcfb2dad680319
hiriorima <g2117001@fun.ac.jp>
2017-11-09 11:05:58 +0900

    Add servo model

--- servo/models.py
+++ servo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Servo(models.Model):
+    status = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
